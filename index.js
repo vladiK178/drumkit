af2d063fffb78fb0a1401cec17fd16edb284ef2f
Vladislav Kraemer <v21kraemer@gmail.com>
2025-01-09 13:58:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+const numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
